0e8f4b61df1a783622bcc145b1509eab5081171e
arthur <arthur.fernandes@sonner.com.br>
2025-06-03 13:46:11 -0300

    Diretiva *ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgSwitchCaseComponent } from './diretiva-ng-switch-case/diretiva-ng-switch-case.component';
 import { DiretivaNgForComponent } from './diretiva-ng-for/diretiva-ng-for.component';
+import { DiretivaNgClassComponent } from './diretiva-ng-class/diretiva-ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgSwitchCaseComponent,
-    DiretivaNgForComponent
+    DiretivaNgForComponent,
+    DiretivaNgClassComponent
   ],
   imports: [
     BrowserModule,
