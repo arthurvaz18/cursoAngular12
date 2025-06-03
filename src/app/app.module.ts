5770ec98058330ad522c9ebb77cad6cf5791d4a4
arthur <arthur.fernandes@sonner.com.br>
2025-06-03 09:35:02 -0300

    Diretiva *ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = [];
+
+  palmeiras: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
